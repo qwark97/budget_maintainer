f5762220c98f5e998a67e11dc433f16e786fd633
qwark97 <marcin.plata1.konta@gmail.com>
2021-07-02 20:16:17 +0200

    add mocked db

---
+++ dbManager.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+var (
+	mockedDBMutex = sync.Mutex{}
+	mockedDB      = make(map[int]Operation)
+	lastMockedID  = 0
+)
+
+func saveOperation(data Operation) error {
+	mockedDBMutex.Lock()
+	defer mockedDBMutex.Unlock()
+	lastMockedID++
+	id := lastMockedID
+	data.ID = id
+	data.Timestamp = int(time.Now().Unix())
+	mockedDB[id] = data
+	return nil
+}
+
+func eraseOperation(id int) error {
+	if _, ok := mockedDB[id]; ok {
+		mockedDBMutex.Lock()
+		defer mockedDBMutex.Unlock()
+		delete(mockedDB, id)
+		return nil
+	} else {
+		return fmt.Errorf("no such id")
+	}
+
+}
+
+func loadAllOperations() (Operations, error) {
+	operations := Operations{}
+	for _, o := range mockedDB {
+		operations = append(operations, o)
+	}
+	return operations, nil
+}
