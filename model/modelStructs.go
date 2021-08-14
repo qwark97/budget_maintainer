86647ae9cbd89fd09b9e075f9f17434a759f19fa
qwark97 <marcin.plata1.konta@gmail.com>
2021-08-14 23:17:55 +0200

    implement helper method to merge received categories

--- model/modelStructs.go
+++ model/modelStructs.go
@@ -1,6 +1,7 @@
 package model
 
 import (
+	"fmt"
 	"time"
 )
 
@@ -22,4 +23,34 @@ type transitBudgetPosition struct {
 
 type manyBudgetPositions struct {
 	elements []budgetPosition
+}
+
+func (mbp *manyBudgetPositions) mergeCategories() {
+	if len(mbp.elements) < 1 {
+		return
+	}
+	var tmpElements []*budgetPosition
+	var mergedElements []budgetPosition
+	var categories = make(map[string]func() func(amount float64) *budgetPosition)
+
+	for i := range mbp.elements {
+		fmt.Println(mbp.elements[i].Category)
+		if f, ok := categories[mbp.elements[i].Category]; ok {
+			f()(mbp.elements[i].Amount)
+		} else {
+			bp := &mbp.elements[i]
+			tmpElements = append(tmpElements, bp)
+			categories[mbp.elements[i].Category] = func() func(amount float64) *budgetPosition {
+				return func(amount float64) *budgetPosition {
+					bp.Amount = bp.Amount + amount
+					return bp
+				}
+			}
+			categories[mbp.elements[i].Category]()(0)
+		}
+	}
+	for _, e := range tmpElements {
+		mergedElements = append(mergedElements, *e)
+	}
+	mbp.elements = mergedElements
 }
