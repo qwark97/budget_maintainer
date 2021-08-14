9f9ac40087b00f122cd3ec23f75f349c8845626f
qwark97 <marcin.plata1.konta@gmail.com>
2021-08-14 23:22:15 +0200

    implement adding new budget

--- model/budget.go
+++ model/budget.go
@@ -11,6 +11,34 @@ func NewTransitBudget(body io.ReadCloser) (transitBudget, error) {
 	return data, err
 }
 
-func SetBudget(data transitBudget) error { return nil }
+func SetBudget(data transitBudget) error {
+	budgetPositions := manyBudgetPositions{}
+	year := data.Year
+	month := data.Month
+
+	for _, transitPosition := range data.BudgetPositions {
+		p := budgetPosition{
+			Category: transitPosition.Category,
+			Amount:   transitPosition.Amount,
+			Year:     year,
+			Month:    month,
+			Group:    transitPosition.Group,
+		}
+		budgetPositions.elements = append(budgetPositions.elements, p)
+	}
+	budgetPositions.mergeCategories()
+	if removeRes := DBConn.
+		Where("year = ?", year).
+		Where("month = ?", month).
+		Unscoped().
+		Delete(&budgetPosition{}); removeRes.Error != nil {
+		return removeRes.Error
+	}
+	for _, position := range budgetPositions.elements {
+		DBConn.Create(&position)
+	}
+
+	return nil
+}
 
 func LoadBudget(year, month int) (transitBudget, error) { return transitBudget{}, nil }
