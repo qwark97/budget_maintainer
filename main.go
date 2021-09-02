c213198ea4d8b475d17c4892c965c41d8043e963
qwark97 <marcin.plata1.konta@gmail.com>
2021-09-02 21:35:14 +0200

    parametrize the app

--- main.go
+++ main.go
@@ -2,23 +2,26 @@ package main
 
 import (
 	"fmt"
-	"github.com/gorilla/handlers"
-	"github.com/qwark97/budget_maintainer/model"
 	"log"
 	"net/http"
 	"os"
 	"time"
 
+	"github.com/gorilla/handlers"
+	"github.com/qwark97/budget_maintainer/model"
+
 	"github.com/gorilla/mux"
 )
 
-const (
-	HOST = "127.0.0.1"
-	PORT = "9999"
+var (
+	HOST   string
+	PORT   string
+	dbConf model.DBConf
 )
 
 func main() {
-	model.InitDatabase()
+	loadConfiguration()
+	model.InitDatabase(dbConf)
 	db, _ := model.DBConn.DB()
 	defer db.Close()
 	router := mux.NewRouter()
@@ -57,4 +60,48 @@ func logSystemErr(err error) {
 	if err != nil {
 		log.Printf("ERROR - %s", err.Error())
 	}
+}
+
+func loadConfiguration() {
+	HOST = os.Getenv("BM_HOST")
+	if HOST == "" {
+		HOST = "127.0.0.1"
+	}
+
+	PORT = os.Getenv("BM_PORT")
+	if PORT == "" {
+		PORT = "9999"
+	}
+
+	dbHost := os.Getenv("BM_DB_HOST")
+	if dbHost == "" {
+		dbHost = "postgres"
+	}
+	dbConf.Host = dbHost
+
+	dbPort := os.Getenv("BM_DB_PORT")
+	if dbPort == "" {
+		dbPort = "5432"
+	}
+	dbConf.Port = dbPort
+
+	dbUser := os.Getenv("BM_DB_USER")
+	if dbUser == "" {
+		dbUser = "postgres"
+	}
+	dbConf.User = dbUser
+
+	dbConf.Pass = os.Getenv("BM_DB_PASS")
+
+	dbName := os.Getenv("BM_DB_NAME")
+	if dbName == "" {
+		dbName = "postgres"
+	}
+	dbConf.Name = dbName
+
+	dbTZ := os.Getenv("TZ")
+	if dbTZ == "" {
+		dbTZ = "Europe/Warsaw"
+	}
+	dbConf.TZ = dbTZ
 }
--- model/database.go
+++ model/database.go
@@ -2,21 +2,21 @@ package model
 
 import (
 	"fmt"
+	"log"
+	"time"
+
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
-	"log"
-	"os"
-	"time"
 )
 
 var (
 	DBConn     *gorm.DB
-	dsnPattern = "host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Europe/Warsaw"
+	dsnPattern = "host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=%s"
 )
 
-func InitDatabase() {
+func InitDatabase(conf DBConf) {
 	var err error
-	dsn := fmt.Sprintf(dsnPattern, "localhost", "postgres", os.Getenv("DB_PASS"), "postgres", "5432")
+	dsn := fmt.Sprintf(dsnPattern, conf.Host, conf.User, conf.Pass, conf.Name, conf.Port, conf.TZ)
 	for {
 		DBConn, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 		if err != nil {
@@ -37,4 +37,13 @@ func InitDatabase() {
 		panic(fmt.Sprintf("cannot migrate: %s", err.Error()))
 	}
 	log.Println("Auto migration succeeded")
+}
+
+type DBConf struct {
+	Host string
+	Port string
+	User string
+	Pass string
+	Name string
+	TZ   string
 }
